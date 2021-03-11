5dd690cf94181735b64acacbc504ae91a6a40779
steffanynaranjov <steffany.naranjo@gmail.com>
2021-03-11 00:53:47 -0500

    some change in the local storage

--- src/storage/useLocalStorage.js
+++ src/storage/useLocalStorage.js
@@ -1,24 +1,23 @@
-import { useEffect, useState } from 'react'
+import { useState } from "react";
 
-const PREFIX = 'code-skills'
-
-export default function useLocalStorage(key, initialValue) {
-  const prefixedKey = PREFIX + key
-
-  const [value, setValue] = useState(() => {
-    const jsonValue = localStorage.getItem(prefixedKey)
-    if (jsonValue != null) return JSON.parse(jsonValue)
-
-    if (typeof initialValue === 'function') {
-      return initialValue()
-    } else {
-      return initialValue
+export function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      return initialValue;
     }
-  })
+  });
 
-  useEffect(() => {
-    localStorage.setItem(prefixedKey, JSON.stringify(value))
-  }, [prefixedKey, value])
+  const setValue = (value) => {
+    try {
+      setStoredValue(value);
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  return [value, setValue]
+  return [storedValue, setValue];
 }
