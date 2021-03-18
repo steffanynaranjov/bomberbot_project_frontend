7e10d7b54d2ef81db236b2af9c07ff159e88ae2e
steffanynaranjov <steffany.naranjo@gmail.com>
2021-03-18 00:45:17 -0500

    adding footer

--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 };
